111af187d45e317c8741bca626180eac86e86d9d
Hyun Heechan <hyeonhc328@gmail.com>
2025-08-12 10:10:51 +0900

    feat(vote): Add VoteRepository and integrate with ViewModel
    This commit introduces `VoteRepository` to manage vote data and integrates it with `VoteListViewModel`.
    
    - Added `VoteRepository` interface and `FirebaseVoteRepository` implementation for Firebase Firestore and Storage operations.
    - Updated `VoteListViewModel` to use `VoteRepository` for observing, adding, and updating votes.

--- app/src/main/java/com/example/clazzi/viewmodel/VoteListViewModel.kt
+++ app/src/main/java/com/example/clazzi/viewmodel/VoteListViewModel.kt
@@ -2,27 +2,30 @@ package com.example.clazzi.viewmodel
 
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.clazzi.model.Vote
-import com.example.clazzi.model.VoteOption
+import com.example.clazzi.repository.FirebaseVoteRepository
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.FieldValue
-import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
-import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import java.util.UUID
 
 class VoteListViewModel : ViewModel() {
     val db = Firebase.firestore
+    val voteRepository = FirebaseVoteRepository()
 
     private val _voteList = MutableStateFlow<List<Vote>>(emptyList())
     val voteList: StateFlow<List<Vote>> = _voteList
+
+    init {
+        viewModelScope.launch {
+            voteRepository.observeVote().collect { votes ->
+                _voteList.value = votes
+            }
+        }
+    }
 
     fun getVoteById(voteId: String): Vote? {
         return voteList.value.find { it.id == voteId }
@@ -30,41 +33,7 @@ class VoteListViewModel : ViewModel() {
 
     fun addVote(vote: Vote, context: Context, imageUri: Uri) {
         viewModelScope.launch {
-
-            try {
-                val storageRef = FirebaseStorage.getInstance().reference
-                val imageRef = storageRef.child("images/${UUID.randomUUID()}.jpg")
-
-                // 이미지 업로드
-                val inputStream = context.contentResolver.openInputStream(imageUri)
-                val uploadTask = inputStream?.let { imageRef.putStream(it).await() }
-
-                // 다운로드 url 가져오기
-                val downloadUrl = imageRef.downloadUrl.await().toString()
-
-
-                val voteMap = hashMapOf(
-                    "id" to vote.id,
-                    "title" to vote.title,
-                    "imageUrl" to downloadUrl,
-                    "createAt" to FieldValue.serverTimestamp(),
-                    "options" to vote.options.map {
-                        hashMapOf(
-                            "id" to it.id,
-                            "optionText" to it.optionText,
-                        )
-                    }
-                )
-                db.collection("votes")
-                    .document(vote.id)
-                    .set(voteMap)
-                    .await()
-                Log.d("Firebase", "Vote added successfully")
-//                db.collection("votes")
-//                    .add(vote)
-            } catch (e: Exception) {
-                Log.e("Firebase", "Error adding vote", e)
-            }
+            voteRepository.addVote(vote, context, imageUri)
         }
 
 
@@ -72,15 +41,7 @@ class VoteListViewModel : ViewModel() {
 
     fun setVote(vote: Vote) {
         viewModelScope.launch {
-            try {
-                db.collection("votes")
-                    .document(vote.id)
-                    .set(vote)
-                    .await()
-                Log.d("Firebase", "Vote updating successfully")
-            } catch (e: Exception) {
-                Log.e("Firebase", "Error updating vote", e)
-            }
+            voteRepository.setVote(vote)
         }
     }
 
@@ -116,17 +77,5 @@ class VoteListViewModel : ViewModel() {
         )
 
     }*/
-    init {
-        db.collection("votes")
-            .orderBy("createAt", Query.Direction.DESCENDING)
-            .addSnapshotListener { snapshot, error ->
-                if (error != null) {
-                    // 오류 처리
-                    return@addSnapshotListener
-                }
-                if (snapshot != null) {
-                    _voteList.value = snapshot.toObjects(Vote::class.java)
-                }
-            }
-    }
+
 }
